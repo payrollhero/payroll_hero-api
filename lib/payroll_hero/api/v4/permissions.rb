94e48791631ac6dc2eaff8956a42553a791e6b1e
Myk <mkyryk@payrollhero.com>
2015-12-28 15:36:05 +0800

    Add v4/permissions

---
+++ lib/payroll_hero/api/v4/permissions.rb
@@ -0,0 +1,13 @@
+module PayrollHero
+  module Api
+    module V4
+      # Permissions Endpoint
+      class Permissions < BaseGroup
+        def list
+          params = {}
+          client.get("/api/v4/permissions", params)
+        end
+      end
+    end
+  end
+end
