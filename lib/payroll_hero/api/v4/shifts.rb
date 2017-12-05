a71d54b74325124f4e9f584e942ed1083d193e0f
Ronald Maravilla <more.ron.too@gmail.com>
2017-12-04 18:37:56 -0800

    Allow passing of params to shift's list

--- lib/payroll_hero/api/v4/shifts.rb
+++ lib/payroll_hero/api/v4/shifts.rb
@@ -2,8 +2,8 @@ module PayrollHero
   module Api
     module V4
       class Shifts < BaseGroup
-        def list
-          client.get('/api/v4/shifts')
+        def list(**params)
+          client.get '/api/v4/shifts', params
         end
       end
     end
