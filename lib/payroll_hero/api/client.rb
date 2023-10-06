b43f102d3069da83474373cced3bf7f561c81ce5
P.T <lymaeq+github@gmail.com>
2023-10-06 20:14:13 +0200

    replace present with !empty

--- lib/payroll_hero/api/client.rb
+++ lib/payroll_hero/api/client.rb
@@ -12,7 +12,7 @@ module PayrollHero
         headers = {
           'User-Agent' => user_agent_string,
         }
-        if cookies.present?
+        unless cookies.empty?
           headers.merge!({ 'Cookie' => (cookies.map{ |key, value| "#{key}=#{value}" }.join('; ')) })
         end
         params = {
